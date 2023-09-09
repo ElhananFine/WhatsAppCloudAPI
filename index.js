3d4b0a9c08da6933fd7733e561a120fe7e0b9915
Elhanan Fine <elhananfine@h-mhsb-nyyd-sl-Elhanan.local>
2023-09-09 22:22:46 +0300

    התחייבות ראשונית

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Whatsapp } = require("./src/Client");
 // prettier-ignore
-const {markAsRead, MessageText, List, Buttoms, ImageByID} = require("./src/Models/Template")
+const {document, sticker, contacts, markAsRead, MessageText, List, Buttoms, Image, ReplyEmojyToMsg, audio, location, video} = require("./src/Models/Template")
 const { parsePhoneNumbers } = require("./src/Controllers/utils");
 
 // prettier-ignore
-module.exports = { Whatsapp, markAsRead, MessageText, List, Buttoms, parsePhoneNumbers };
+module.exports = { document, sticker, contacts, markAsRead, MessageText, List, Buttoms, Image, ReplyEmojyToMsg, audio, location, video, parsePhoneNumbers  };
 
 ////////////////////////////////////////////////////////////////
--- test.js
+++
@@ -1,8 +0,0 @@
-const isLinkOrID = (inputString) => {
-  if (/^\d+$/.test(inputString)) return "ID";
-  if (/^(https?|www)/i.test(inputString)) return "Link";
-  return false;
-};
-
-console.log(isLinkOrID("www.elhananfine.com")); // ידפיס "ID"
-console.log(isLinkOrID("972556866871")); // ידפיס "Unknown"
